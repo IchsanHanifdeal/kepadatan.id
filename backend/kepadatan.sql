ad799135fb1eb74b63d9c39224655887f90f0fb2
IchsanHanifdeal <108577976+IchsanHanifdeal@users.noreply.github.com>
2023-12-05 15:36:04 +0700

    [ADD] Database

--- backend/kepadatan.sql
+++ backend/kepadatan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 02 Des 2023 pada 22.44
--- Versi server: 10.4.27-MariaDB
--- Versi PHP: 7.4.33
+-- Waktu pembuatan: 05 Des 2023 pada 09.35
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -32,16 +32,17 @@ CREATE TABLE `kepadatan` (
   `nama_daerah` varchar(255) NOT NULL,
   `latitude` varchar(255) NOT NULL,
   `longitude` varchar(255) NOT NULL,
-  `kepadatan` varchar(255) NOT NULL
+  `level` enum('rendah','menengah','tinggi') NOT NULL,
+  `radius` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `kepadatan`
 --
 
-INSERT INTO `kepadatan` (`id`, `nama_daerah`, `latitude`, `longitude`, `kepadatan`) VALUES
-(1, 'Halte Gramedia Mall Pekanbaru, Kota Tinggi, Kec. Pekanbaru Kota, Kota Pekanbaru, Riau, Indonesia', '0.5319709639530052', '101.44792556762695', '50000'),
-(2, 'Jl. Ir. H. Juanda No.29, Kp. Dalam, Kec. Senapelan, Kota Pekanbaru, Riau 28155, Indonesia', '0.5358057917720732', '101.44667038639416', '1000');
+INSERT INTO `kepadatan` (`id`, `nama_daerah`, `latitude`, `longitude`, `level`, `radius`) VALUES
+(2, 'Jl. Ir. H. Juanda No.29, Kp. Dalam, Kec. Senapelan, Kota Pekanbaru, Riau 28155, Indonesia', '0.5358057917720732', '101.44667038639416', 'rendah', 100),
+(3, 'Jl. Jend. Sudirman No.395, Simpang Empat, Kec. Pekanbaru Kota, Kota Pekanbaru, Riau 28121, Indonesia', '0.5127456888292616', '101.44844055175781', 'menengah', 1000);
 
 -- --------------------------------------------------------
 
@@ -88,7 +89,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `kepadatan`
 --
 ALTER TABLE `kepadatan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
